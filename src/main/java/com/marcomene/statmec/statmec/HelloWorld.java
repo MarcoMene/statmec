c22e20aa968d4bbc2b750b6559e32a5158542cfd
u0h2247 <marco.meneghelli@esternibisp.com>
2015-04-24 12:09:02 +0200

    first commit

---
+++ src/main/java/com/marcomene/statmec/statmec/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.marcomene.statmec.statmec;
+
+/**
+ * Created by u0h2247 on 4/24/2015.
+ */
+public class HelloWorld {
+
+    public static void helloWorld(){
+        System.out.print("Hello world!");
+    }
+}
---
+++ src/test/com/marcomene/statmec/statmec/HelloWorldTest.java
@@ -0,0 +1,10 @@
+package com.marcomene.statmec.statmec;
+
+import junit.framework.TestCase;
+
+public class HelloWorldTest extends TestCase {
+
+    public void testHelloWorld() throws Exception {
+        HelloWorld.helloWorld();
+    }
+}
